abbd25f3b8aeb1faaa65fc83cad0783afbb69ef8
Pauline <paulineh@github.com>
2018-04-03 18:32:59 -0500

    updated hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,48 +1,50 @@
-  "use strict"
+"use strict"
 
-  $(document).ready(function(){
-
-  }
-    var count = 0
-
-    $('.draggable').draggable({
-      revert: "invalid"
-    });
+$(document).ready(function(){
 
 
+  // var count = 0
 
-    $('#drop1, #drop2, #drop3').droppable({
-     //event = what happening
-     //ui = the item that's being drop
-      drop: function(event,ui){
-        count++
-        var start = $(ui.draggable).data('block');
-        var last = $(this).children().last().data('block');
-          if ( start > last ) {
-
-      // ui.draggable.draggable( 'disable' );
-      // $(this).droppable( 'disable', 'revert', true );
-
-
-      $(ui.draggable).draggable( 'option', 'revert', true );
-          }else{
-
-        // console.log(`current block ${start}`);
-        // console.log('last block ${last}`);
-        $(ui.draggable).appendTo(this).attr('style', 'position:"relative"');
-
-
-          }
-
-     checkWin();
-      }
-      function checkWin() {
-      if($('[data-stack=3]').children().length == 4){
-        $('announce-game-won').text(`Winner!`);
-      }
-    });
+  $(".draggable").draggable({
+    revert: "invalid"
   });
 
-  $(".clear").draggable(
-     { refreshPositions: true }
-  );
+
+
+  $('#drop1, #drop2, #drop3').droppable({
+   //event = what happening
+   //ui = the item that's being drop
+    drop: function(event,ui){
+      count++
+      var start = $(ui.draggable).data('block');
+      var last = $(this).children().last().data('block');
+        if ( start > last ) {
+
+    // ui.draggable.draggable( 'disable' );
+    // $(this).droppable( 'disable', 'revert', true );
+
+
+    $(ui.draggable).draggable( 'option', 'revert', true );
+        }else{
+
+      // console.log(`current block ${start}`);
+      // console.log('last block ${last}`);
+      $(ui.draggable).appendTo(this).attr('style', 'position:relative');
+
+        }
+        }
+
+   // checkWin();
+
+  //   function checkWin() {
+  //   if($('[data-stack=3]').children().length == 4){
+  //     $('announce-game-won').text(`Winner!`);
+  //   }
+  // });
+});
+
+
+$(".clear").draggable(
+   { refreshPositions: true }
+);
+})
